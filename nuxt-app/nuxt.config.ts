72ad3795114d278363ff372eb7e97949b42ea063
Gonzalo Encinar <gonzalo.encinar.contractor@bbva.com>
2024-08-08 23:01:53 +0200

    Nuxt Studio Setup

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+    "@nuxthq/studio",
+  ],
   colorMode: {
     classSuffix: "",
   },
